7600aeff845f4dd5016bb78485392dd3d9f58612
Tim Destan <tim.destan@gmail.com>
2025-06-19 00:23:18 -0400

    Remove unnecessary command
    Largely redundant with revert-buffer-quick

--- elisp/my-custom-commands.el
+++ elisp/my-custom-commands.el
@@ -6,9 +6,3 @@
   (if (use-region-p)
       (call-interactively 'kill-region)
     (backward-kill-word arg)))
-
-;; Source: https://www.emacswiki.org/emacs/misc-cmds.el
-(defun revert-buffer-no-confirm ()
-  "Revert buffer without confirmation."
-  (interactive)
-  (revert-buffer :ignore-auto :noconfirm))
