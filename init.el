8d5d83f3053d5501d1fe1dfd907d2dea21d22032
Tim Destan <tim.destan@gmail.com>
2025-12-20 20:15:23 -0500

    Trust all custom themes

--- init.el
+++ init.el
@@ -53,6 +53,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes t)
  '(custom-enabled-themes '(dark-blue2))
  '(indent-tabs-mode nil)
  '(package-selected-packages
