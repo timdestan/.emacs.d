c07812942f9bc1e666dbc289cebdbd58b1b099ab
Tim Destan <tim.destan@gmail.com>
2025-12-20 23:26:59 -0500

    company mode

--- init.el
+++ init.el
@@ -47,11 +47,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes t)
  '(custom-enabled-themes '(dark-blue2))
+ '(custom-safe-themes t)
  '(indent-tabs-mode nil)
  '(package-selected-packages
-   '(flycheck lsp-mode mwim markdown-mode rust-mode haskell-mode rainbow-delimiters git-gutter exec-path-from-shell use-package))
+   '(company flycheck lsp-mode mwim markdown-mode rust-mode haskell-mode rainbow-delimiters git-gutter exec-path-from-shell use-package))
  '(standard-indent 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
